137e7ebc5b5d7e9a784958d7d832564e025268e5
marwanzakhour <marwanzakhour@gmail.com>
2015-10-26 15:22:01 +0200

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$host = "mysql12.000webhost.com";
+$user = "a3736562_marwan";
+$password = "chocomaxx3adjej";
+$db_name = "a3736562_marwan";
+
+$mysqli = new mysqli($host, $user, $password, $db_name);
+
+if(mysqli_connect_errno())
+{
+	printf("Connection failed");
+	exit();
+} 
+?>
